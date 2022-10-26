e3310d2558661f6a65f0e874ff75e5671ea55eef
Nils Niggemann <niggeni@zedat.fu-berlin.de>
2022-10-26 16:55:40 +0200

    latex option for ticks

--- src/Ticks.jl
+++ src/Ticks.jl
@@ -1,13 +1,20 @@
-function simplePiTicks(n,min,max;sigdigits=1)
+function simplePiTicks(n,min,max;sigdigits=1,latex = true)
     range = LinRange(min,max,n)
+    toString,zero_string,pi_string = 
+    if latex
+        latexstring,L"0",L"\pi"
+    else
+        string,"0","π"
+    end
+
     function simplify_pi(x)
-        x = x/pi
-        if x == 0
-            return L"0"
+        x = x/π
+        if x ≈ 0.
+            return zero_string
         elseif isinteger(x)
-            return latexstring(Int(x),L"\pi")
+            return toString(Int(x),pi_string)
         else
-            return latexstring(round(x;sigdigits),L"\pi")
+            return toString(round(x;sigdigits),pi_string)
         end
     end
     ticks = simplify_pi.(range)
